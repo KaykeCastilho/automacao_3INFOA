0c74dba36dcddd663f603b409722dc4879a0c198
KaykeCastilho <kayke.lima@alunos.ifsuldeminas.edu.br>
2024-02-19 10:13:48 -0300

    Aula da semana1

---
+++ semana1/exemplo2_entrada.py
@@ -0,0 +1,5 @@
+#Entrada
+n1 = variavel1 = input("Digite o número1: ")
+n2 = Variavel1 = input("Digite o número2: ")
+#Saída
+print("Você digitou", n1, n2)
---
+++ semana1/exemplo3_processamento.py
@@ -0,0 +1,16 @@
+#Entrada
+n1 = variavel1 = input("Digite o número1: ")
+n2 = Variavel1 = input("Digite o número2: ")
+
+
+#Processamento
+
+soma = int(n1) + int(n2)
+
+
+#Saída
+print("A soma é de", n1, " + ", n2, " = ", soma)
+
+# int("10") -> converte o texto "10" para inteiro 10
+# int("10.5") -> converte o texto "10.5" para real 1.5
+#str(10) -> converte o inteiro 10 para texto "10"
